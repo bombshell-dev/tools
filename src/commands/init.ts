5857a293d106ec48dc22adf92a4e7031d750d01b
Nate Moore <nate@natemoo.re>
2025-03-30 21:01:50 -0500

    refactor: cleanup init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -2,12 +2,11 @@ import { readFile, writeFile } from "node:fs/promises";
 import { dirname, sep } from "node:path";
 import { cwd } from "node:process";
 import { pathToFileURL } from "node:url";
-import { parse } from "@bomb.sh/args";
 import { x } from "tinyexec";
 import type { CommandContext } from "../context.ts";
 
 export async function init(ctx: CommandContext) {
-	const [_name = '.', ...args] = ctx.args;
+	const [_name = '.'] = ctx.args;
 	const name = _name === '.' ? dirname(cwd()) : _name;
 	const dest = new URL("./.temp/", pathToFileURL([cwd(), sep].join("")));
 	for await (const line of x("pnpx", ["giget@latest", "gh:bombshell-dev/template", name])) {
